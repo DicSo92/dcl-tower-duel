8a2ddea1d65fecee741a1247970eb66b33682437
Charly <luzzi.charly@gmail.com>
2022-05-18 23:39:11 +0200

    game Class

--- src/game.ts
+++ src/game.ts
@@ -2,30 +2,54 @@ import BlueButton from "@/blueButton";
 import TowerDuel from "@/towerDuel";
 import { loadColliders } from "@/colliderSetup";
 
-// Setup our world
-const world = new CANNON.World()
-world.quatNormalizeSkip = 0
-world.quatNormalizeFast = false
-world.gravity.set(0, -9.82, 0) // m/s²
-loadColliders(world)
-// Setup ground material
-const physicsMaterial = new CANNON.Material("groundMaterial")
-const ballContactMaterial = new CANNON.ContactMaterial(physicsMaterial, physicsMaterial, { friction: 1, restitution: 0.5 })
-world.addContactMaterial(ballContactMaterial)
-
 onSceneReadyObservable.add(() => {
     log("SCENE LOADED");
-    const messageBus = new MessageBus()
-
-    const blueButton = new BlueButton(new Transform({
-        position: new Vector3(3, 1.1, 3),
-        rotation: new Quaternion(0, 0, 0, 1),
-        scale: new Vector3(2, 2, 2)
-    }), messageBus);
-
-    messageBus.on("blueButtonClick", (test) => {
-        log('new Game')
-        const game = new TowerDuel(physicsMaterial, world, messageBus)
-    })
-    engine.addSystem(blueButton);
+    const game = new Game()
+    engine.addSystem(game);
 });
+
+export default class Game implements ISystem {
+    physicsMaterial: CANNON.Material
+    world: CANNON.World
+    messageBus: MessageBus
+    TowerDuel?: TowerDuel
+
+    constructor() {
+        this.physicsMaterial = new CANNON.Material("groundMaterial")
+        this.world = new CANNON.World()
+        this.messageBus = new MessageBus()
+
+        this.SetupWorldConfig()
+        this.buildScene()
+        this.BuildEvents()
+    }
+
+    private SetupWorldConfig() {
+        this.world.quatNormalizeSkip = 0
+        this.world.quatNormalizeFast = false
+        this.world.gravity.set(0, -9.82, 0) // m/s²
+        loadColliders(this.world)
+        const ballContactMaterial = new CANNON.ContactMaterial(this.physicsMaterial, this.physicsMaterial, { friction: 1, restitution: 0.5 })
+        this.world.addContactMaterial(ballContactMaterial)
+    }
+
+    private buildScene() {
+        const blueButton = new BlueButton(new Transform({
+            position: new Vector3(3, 1.1, 3),
+            rotation: new Quaternion(0, 0, 0, 1),
+            scale: new Vector3(2, 2, 2)
+        }), this.messageBus);
+
+        engine.addSystem(blueButton);
+    }
+    private BuildEvents() {
+        this.messageBus.on("blueButtonClick", (test) => {
+            log('new Game')
+            this.TowerDuel = new TowerDuel(this.physicsMaterial, this.world, this.messageBus)
+        })
+    }
+
+    update(dt: number): void {
+
+    }
+}
