43b033aa580c2b82fd20df88d784cb7a0fd642d2
Gwennouw <70351609+Gwennouw@users.noreply.github.com>
2022-06-14 19:58:20 +0200

    Fix lift height problem && add spell4 for movePlayerOnLift

--- src/liftToGame.ts
+++ src/liftToGame.ts
@@ -13,12 +13,18 @@ export default class LiftToGame implements ISystem {
     parent: MainGame
     startPos: Vector3
     endPos: Vector3
-    centerPos: Vector3
+    centerPos0: Vector3
+    centerPos1: Vector3
+    centerPos2: Vector3
+    centerPos3: Vector3
+    centerPos4: Vector3
+    centerPos5: Vector3
     startPath: Vector3[]
     endPath: Vector3[]
     state: number = 0 // 0: !isActive; 1: goToPlay; -1: goToLobby
     liftMaxHeight: number = 5
-    liftMoveDuration: number = 5
+    liftCollisionOffset: number = 3
+    liftMoveDuration: number = 8
     isActive: boolean = false
     radius: number = 1
     outOfLift: boolean = false
@@ -26,12 +32,22 @@ export default class LiftToGame implements ISystem {
     constructor(parent: MainGame) {
         this.parent = parent
         if (this.parent.side === 'left') {
-            this.centerPos = new Vector3(28, this.liftMaxHeight, 16)
             this.startPos = new Vector3(24, 0.1, 24)
+            this.centerPos0 = new Vector3(24, this.liftMaxHeight, 24)
+            this.centerPos1 = new Vector3(24, this.liftMaxHeight, 14)
+            this.centerPos2 = new Vector3(30, this.liftMaxHeight, 14)
+            this.centerPos3 = new Vector3(30, this.liftMaxHeight, 8)
+            this.centerPos4 = new Vector3(30, this.liftMaxHeight + this.liftCollisionOffset, 8)
+            this.centerPos5 = new Vector3(30, this.liftMaxHeight + this.liftCollisionOffset, 2)
             this.endPos = new Vector3(30, 3.3, 2)
         } else {
-            this.centerPos = new Vector3(4, this.liftMaxHeight, 16)
             this.startPos = new Vector3(8, 0.1, 24)
+            this.centerPos0 = new Vector3(8, this.liftMaxHeight, 24)
+            this.centerPos1 = new Vector3(8, this.liftMaxHeight, 14)
+            this.centerPos2 = new Vector3(2, this.liftMaxHeight, 14)
+            this.centerPos3 = new Vector3(2, this.liftMaxHeight, 8)
+            this.centerPos4 = new Vector3(2, this.liftMaxHeight + this.liftCollisionOffset, 8)
+            this.centerPos5 = new Vector3(2, this.liftMaxHeight + this.liftCollisionOffset, 2)
             this.endPos = new Vector3(2, 3.3, 2)
         }
         this.lift = new Entity()
@@ -46,14 +62,24 @@ export default class LiftToGame implements ISystem {
         this.startPath = [
             this.startPos,
             new Vector3(this.startPos.x, this.liftMaxHeight, this.startPos.z),
-            this.centerPos,
+            this.centerPos0,
+            this.centerPos1,
+            this.centerPos2,
+            this.centerPos3,
+            this.centerPos4,
+            this.centerPos5,
             new Vector3(this.endPos.x, this.liftMaxHeight, this.endPos.z),
             this.endPos
         ]
         this.endPath = [
             new Vector3(this.endPos.x, this.lift.getComponent(Transform).position.y, this.endPos.z),
             new Vector3(this.endPos.x, this.liftMaxHeight, this.endPos.z),
-            this.centerPos,
+            this.centerPos5,
+            this.centerPos4,
+            this.centerPos3,
+            this.centerPos2,
+            this.centerPos1,
+            this.centerPos0,
             new Vector3(this.startPos.x, this.liftMaxHeight, this.startPos.z),
             this.startPos,
         ]
@@ -93,7 +119,7 @@ export default class LiftToGame implements ISystem {
         this.isActive = true
         this.state = -1
         this.lift.getComponent(GLTFShape).visible = true
-        this.parent.TowerDuel?.lift?.lift.getComponent(GLTFShape).withCollisions ? this.parent.TowerDuel.lift.lift.getComponent(GLTFShape).withCollisions  = false : ''
+        this.parent.TowerDuel?.lift?.lift.getComponent(GLTFShape).withCollisions ? this.parent.TowerDuel.lift.lift.getComponent(GLTFShape).withCollisions = false : ''
         // this.parent.TowerDuel?.lift?.Delete()
         return this.entity.addComponentOrReplace(new utils.FollowPathComponent(this.endPath, this.liftMoveDuration, () => {
             this.isActive = false
@@ -105,25 +131,25 @@ export default class LiftToGame implements ISystem {
     }
 
     update(dt: number) {
-    //     if (this.isActive && this.state !== 0) {
-    //         const userOutOfLiftY = Camera.instance.feetPosition.y < this.entity.getComponent(Transform).position.y - 1.5 || Camera.instance.position.y > this.entity.getComponent(Transform).position.y + 10
-    //         if (userOutOfLiftY) {
-    //             movePlayerTo(this.state === 1 ? new Vector3(this.endPos.x, this.endPos.y + 3, this.endPos.z) : this.startPos, this.state === 1 ? this.startPos : new Vector3(this.endPos.x, this.endPos.y + 1.4, this.endPos.z))
-    //             if (this.state === 1) {
-    //                 if (this.lift.getComponent(GLTFShape).visible !== false) {
-    //                     this.lift.getComponent(GLTFShape).visible = false
-    //                     this.entity.getComponent(GLTFShape).visible = false
-    //                 }
-    //                 this.state = 0
-    //                 this.isActive = false
-    //                 engine.removeSystem(this)
-    //             } else if (this.state === -1) {
-    //                 this.isActive = false
-    //                 this.state = 0
-    //                 this.entity.removeComponent(GLTFShape)
-    //                 engine.removeSystem(this)
-    //             }
-    //         }
-    //     }
+        //     if (this.isActive && this.state !== 0) {
+        //         const userOutOfLiftY = Camera.instance.feetPosition.y < this.entity.getComponent(Transform).position.y - 1.5 || Camera.instance.position.y > this.entity.getComponent(Transform).position.y + 10
+        //         if (userOutOfLiftY) {
+        //             movePlayerTo(this.state === 1 ? new Vector3(this.endPos.x, this.endPos.y + 3, this.endPos.z) : this.startPos, this.state === 1 ? this.startPos : new Vector3(this.endPos.x, this.endPos.y + 1.4, this.endPos.z))
+        //             if (this.state === 1) {
+        //                 if (this.lift.getComponent(GLTFShape).visible !== false) {
+        //                     this.lift.getComponent(GLTFShape).visible = false
+        //                     this.entity.getComponent(GLTFShape).visible = false
+        //                 }
+        //                 this.state = 0
+        //                 this.isActive = false
+        //                 engine.removeSystem(this)
+        //             } else if (this.state === -1) {
+        //                 this.isActive = false
+        //                 this.state = 0
+        //                 this.entity.removeComponent(GLTFShape)
+        //                 engine.removeSystem(this)
+        //             }
+        //         }
+        //     }
     }
 }
